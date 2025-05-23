c3b2cd6fbe9d2c6f6f31e5ae44d48ef02c7eb9f2
Nahiyan Kamal <nahiyankamal@Mac.fritz.box>
2025-05-23 23:05:40 +0200

    test message

--- cypress/e2e/url_tests.cy.js
+++ cypress/e2e/url_tests.cy.js
@@ -1,38 +1,36 @@
 describe("Testing Each Documentation Page", () => {
   beforeEach(() => {
-    // Go to docs page before each test
     cy.visit("http://localhost:3000");
-    cy.contains("GET STARTED").click();
   });
 
   it("Install and Quick start page contains correct headlines", () => {
-    cy.contains("Install and Quick start").click();
+    cy.visit("http://localhost:3000/docs");
     cy.contains("Installation");
     cy.contains("Basic Example");
   });
 
   it("Create Table Instance page contains correct headlines", () => {
-    cy.contains("Create Table Instance").click();
+    cy.visit("http://localhost:3000/docs/doc-table-instance-creation");
     cy.contains("Table instance creation");
     cy.contains("Functions of table instance");
   });
 
   it("Adding Rows page contains correct headlines", () => {
-    cy.contains("Adding Rows").click();
+    cy.visit("http://localhost:3000/docs/doc-adding-rows");
     cy.contains("One row at a time");
     cy.contains("Batch Row Adding");
     cy.contains("Chained Row adding");
   });
 
   it("Row Dividers page contains correct headlines", () => {
-    cy.contains("Row Dividers").click();
+    cy.visit("http://localhost:3000/docs/doc-row-divider");
     cy.contains("Basic Row Divider");
     cy.contains("Multiple Sections");
     cy.contains("Using with Computed Values");
   });
 
   it("Adding Columns page contains correct headlines", () => {
-    cy.contains("Adding Columns").click();
+    cy.visit("http://localhost:3000/docs/doc-add-columns");
     cy.contains("Adding Columns One by One");
     cy.contains("Adding Multiple Columns at Once");
     cy.contains("Chained Column Adding");
@@ -40,73 +38,72 @@ describe("Testing Each Documentation Page", () => {
   });
 
   it("Coloring page contains correct headlines", () => {
-    cy.contains("Coloring").click();
+    cy.visit("http://localhost:3000/docs/doc-color");
     cy.contains("Coloring");
     cy.contains("Batch Row coloring");
-    //cy.contains("possible `color` values for rows/columns");
     cy.contains("Custom Color");
   });
 
   it("Sort and Filter page contains correct headlines", () => {
-    cy.contains("Sort and Filter").click();
+    cy.visit("http://localhost:3000/docs/doc-sort-filter");
     cy.contains("Sort");
     cy.contains("Filter");
   });
 
   it("Alignment page contains correct headlines", () => {
-    cy.contains("Alignment").click();
-    //cy.contains("possible `alignment` values for columns");
+    cy.visit("http://localhost:3000/docs/doc-alignment");
+    cy.contains("You can put properties for alignment");
   });
 
   it("Enable and Disable Columns page contains correct headlines", () => {
-    cy.contains("Enable and Disable Columns").click();
+    cy.visit("http://localhost:3000/docs/doc-enable-disable-col");
     cy.contains("Enable");
     cy.contains("Disable");
   });
 
   it("Calculated Columns page contains correct headlines", () => {
-    cy.contains("Calculated Columns").click();
+    cy.visit("http://localhost:3000/docs/doc-computed-function");
     cy.contains("Sometimes you need a new column added automatically whose val is dependent on other fields in the same row");
   });
 
   it("Table Title page contains correct headlines", () => {
-    cy.contains("Table Title").click();
+    cy.visit("http://localhost:3000/docs/doc-title");
     cy.contains("Currently all table Titles will be White Bold style and aligned center");
   });
 
   it("Column Title page contains correct headlines", () => {
-    cy.contains("Column Title").click();
+    cy.visit("http://localhost:3000/docs/doc-column-title");
     cy.contains("You can choose to have your own Column Title instead of using the ugly name of the Column");
   });
 
   it("Limit Column Width page contains correct headlines", () => {
-    cy.contains("Limit Column Width").click();
+    cy.visit("http://localhost:3000/docs/doc-limit-line-width");
     cy.contains("maxLen");
     cy.contains("minLen");
   });
 
   it("Border Design page contains correct headlines", () => {
-    cy.contains("Border Design").click();
+    cy.visit("http://localhost:3000/docs/doc-border-design");
     cy.contains("You can configure the border of the table by passing style in Table constructor");
   });
 
   it("Special Chars and emojis page contains correct headlines", () => {
-    cy.contains("Special Chars and emojis").click();
+    cy.visit("http://localhost:3000/docs/doc-emojis-special-chars");
     cy.contains("Special chars");
   });
 
   it("Render Console Output page contains correct headlines", () => {
-    cy.contains("Render Console Output").click();
+    cy.visit("http://localhost:3000/docs/doc-render-console");
     cy.contains("Output can be generated as a string so that you can choose to use another shell to send the output to");
   });
 
   it("Typescript page contains correct headlines", () => {
-    cy.contains("Typescript").click();
+    cy.visit("http://localhost:3000/docs/doc-typescript");
     cy.contains("You can easily import these in typescript");
   });
 
   it("CLI Quick Start page contains correct headlines", () => {
-    cy.contains("Quick Start").click();
+    cy.visit("http://localhost:3000/docs/doc-cli-install-quick-start");
     cy.contains("Synopsis");
     cy.contains("Installation");
     cy.contains("Basic Example");
